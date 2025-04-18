7e2905486f589f945c67338b00172aaa9b319316
Yoohee Cha <yooheecha@gmail.com>
2025-04-19 00:50:53 +0200

    Image size update

--- randomimg.js
+++ randomimg.js
@@ -1,13 +1,4 @@
-const images = [ "starting/01.jpg", "starting/02.jpg", 
-  "starting/03.png","starting/04.png", "starting/05.JPG", "starting/06.png", "starting/07.png", "starting/08.png", "starting/09.jpg", "starting/10.png", "starting/11.png",
-  "starting/12.png", "starting/13.jpg", "starting/14.png", "starting/15.JPG",
-  "starting/16.png", "starting/17.png","starting/18.png", "starting/19.png", "starting/20.png", "starting/21.png",
-  "starting/22.png","starting/23.jpg", "starting/24.JPG", "starting/25.png", "starting/26.png", "starting/27.png", "starting/28.png",
-  "starting/29.JPG", "starting/30.jpg","starting/31.jpg",
-  "starting/32.jpg","starting/33.jpg", "starting/34.jpg",
-  "starting/35.jpg","starting/36.jpg","starting/34.png","starting/37.jpg", "starting/38.jpg", "starting/39.jpg",
-  "starting/40.jpg","starting/41.jpg","starting/42.png", "starting/43.JPG", "starting/44.jpg", "starting/45.jpg","starting/46.png","starting/47.jpg"
-];
+const images = [ "starting/01.jpg", "starting/02.jpg", "starting/03.jpg", "starting/04.jpg", "starting/05.jpg", "starting/06.jpg", "starting/07.jpg", "starting/08.jpg", "starting/09.jpg", "starting/10.jpg", "starting/11.jpg", "starting/12.jpg", "starting/13.jpg", "starting/14.jpg", "starting/15.jpg", "starting/16.jpg", "starting/17.jpg", "starting/18.jpg", "starting/19.jpg", "starting/20.jpg", "starting/21.jpg", "starting/22.jpg", "starting/23.jpg", "starting/24.jpg", "starting/25.jpg", "starting/26.jpg", "starting/27.jpg", "starting/28.jpg", "starting/29.jpg", "starting/30.jpg", "starting/31.jpg", "starting/32.jpg", "starting/33.jpg", "starting/34.jpg", "starting/35.jpg", "starting/36.jpg", "starting/37.jpg", "starting/38.jpg", "starting/39.jpg", "starting/40.jpg", "starting/41.jpg", "starting/42.jpg", "starting/43.jpg", "starting/44.jpg", "starting/45.jpg", "starting/46.jpg", "starting/47.jpg" ]
 
 let currentIndex = 0;
 let animationActive = true; 
