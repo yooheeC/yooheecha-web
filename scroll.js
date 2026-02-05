0ac366d6cac03072ea5a0f25f76937bf17a4882b
Yoohee Cha <yooheecha@gmail.com>
2026-02-05 15:45:31 +0100

    change layout

--- scroll.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('#scroll-wrapper');
-
-document.addEventListener('wheel', (e) => {
-    e.preventDefault(); // 기본 스크롤 방지
-    container.scrollLeft += e.deltaY; // 세로 스크롤을 가로로 변환
-}, { passive: false }); // passive 옵션 false로 설정
